b4a51fbc87f086b7642f619ea5732eee43d952a4
Brian Trammell <btramm82@gmail.com>
2015-04-29 19:11:18 -0700

    Initial Commit

---
+++ GigMate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GigMate
+//
+//  Created by BRIAN TRAMMELL on 4/29/15.
+//  Copyright (c) 2015 TDesigns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
